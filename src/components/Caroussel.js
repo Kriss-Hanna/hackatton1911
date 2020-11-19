961ca838b19724b3299f5bc88093f7cc8cfb6329
Yang YU <yuyangyyy@gmail.com>
2020-11-19 22:06:57 +0100

    update css

--- src/components/Caroussel.js
+++ src/components/Caroussel.js
@@ -5,14 +5,22 @@ function Caroussel() {
   return (
     <div className="slider">
       <div className="slides">
-        <div className="slide">
-          <img classsrc="./img/images1.jpeg" alt="echange" />
+        <div>
+          <img className="echange" src="./img/images1.jpeg" alt="echange" />
         </div>
-        <div className="slide">
-          <img src="./img/images4.jpeg" alt="img moyen age" />
+        <div>
+          <img
+            className="echange"
+            src="./img/images4.jpeg"
+            alt="img moyen age"
+          />
         </div>
-        <div className="slide">
-          <img src="./img/images3.jpeg" alt="change poisson" />
+        <div>
+          <img
+            className="echange"
+            src="./img/images3.jpeg"
+            alt="change poisson"
+          />
         </div>
       </div>
     </div>
