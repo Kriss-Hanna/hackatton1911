2d2036b48d677708b421127a2ea3d0d300c66cdb
AdelKaced <adel.kaced@gmail.com>
2020-11-19 12:50:35 +0100

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import Nav from './components/Nav'
+import Caroussel from './components/Caroussel'
+import Form from './components/Form'
+import Search from './components/Search'
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
- 
+      <Nav /> 
+      <Caroussel />
+      <Search />
+      <Form />  
     </div>
   );
 }
